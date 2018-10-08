9d0354b127ed691db99dfe7fd8fbb2680c11c16d
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2018-10-08 20:36:43 -0300

    more code

--- build.sbt
+++ build.sbt
@@ -15,8 +15,8 @@ scalaVersion := "2.11.8"
 resolvers += Classpaths.typesafeReleases
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" % "spark-core_2.11" % "2.3.1",
-  "org.apache.spark" % "spark-sql_2.11" % "2.3.1",
+  "org.apache.spark" % "spark-core_2.11" % "2.3.2",
+  "org.apache.spark" % "spark-sql_2.11" % "2.3.2",
   "com.amazonaws" % "aws-java-sdk-s3" % "1.11.401",
   "com.databricks" % "spark-csv_2.11" % "1.5.0",
   scalaTest % Test
--- src/main/scala/org/codecraftlabs/nyc/Main.scala
+++ src/main/scala/org/codecraftlabs/nyc/Main.scala
@@ -1,7 +1,17 @@
 package org.codecraftlabs.nyc
 
+import org.apache.log4j.Level.OFF
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
 object Main {
   def main(args: Array[String]): Unit = {
 
+    @transient lazy val logger = Logger.getLogger(getClass.getName)
+    Logger.getLogger("org.apache").setLevel(OFF)
+
+    logger.info("Processing Kaggle NYC Parking violations data set")
+
+    val sparkSession: SparkSession = SparkSession.builder.appName("kaggle-nyc-parking-violations").master("local[*]").getOrCreate()
   }
 }

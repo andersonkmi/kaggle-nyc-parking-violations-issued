178e5e41df7c1733e58e365e2861ad151f51c44a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2018-10-29 21:51:50 -0300

    code drop

--- src/main/scala/org/codecraftlabs/nyc/Main.scala
+++ src/main/scala/org/codecraftlabs/nyc/Main.scala
@@ -3,6 +3,7 @@ package org.codecraftlabs.nyc
 import org.apache.log4j.Level.OFF
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
+import org.codecraftlabs.nyc.data.ParkingViolation
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,10 +14,14 @@ object Main {
     logger.info("Processing Kaggle NYC Parking violations data set")
 
     val sparkSession: SparkSession = SparkSession.builder.appName("kaggle-nyc-parking-violations").master("local[*]").getOrCreate()
+    import sparkSession.implicits._
 
     val df = ParkingViolationsDataHandler.readContents("parking-violations-issued-fiscal-year-2018.csv", sparkSession)
     val renamedDF = df.toDF(ParkingViolationsDataHandler.ColumnNames: _*)
-    renamedDF.printSchema()
-    renamedDF.show(20)
+    //renamedDF.printSchema()
+    //renamedDF.show(20)
+
+    val violations = renamedDF.as[ParkingViolation]
+    violations.show(4)
   }
 }
